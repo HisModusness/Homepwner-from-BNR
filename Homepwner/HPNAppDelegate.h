4213cee63ce72499ee42d39b362a155d9f684b91
Liam Westby <lw322@nau.edu>
2014-03-23 18:12:04 -0700

    Initial Commit

---
+++ Homepwner/HPNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HPNAppDelegate.h
+//  Homepwner
+//
+//  Created by Liam Westby on 3/23/14.
+//  Copyright (c) 2014 ACM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HPNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Homepwner/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Homepwner
+//
+//  Created by Liam Westby on 3/23/14.
+//  Copyright (c) 2014 ACM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HPNAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HPNAppDelegate class]));
+    }
+}
